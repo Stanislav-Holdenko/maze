6efa7bbe9f2a27a5c2b9c962526e1ad60090ac4b
i474232898 <i474232898@gmail.com>
2014-11-04 23:01:53 +0000

    fixed pack numbers in setBorder

--- src/uk/co/codemachine/mazerun/Generator.java
+++ src/uk/co/codemachine/mazerun/Generator.java
@@ -7,7 +7,7 @@ import java.util.Random;
 public class Generator {
 	private List<Chunk> maze = new LinkedList<Chunk>();
 	private List<Chunk> temp = new LinkedList<Chunk>();
-	private int sizeX, sizeY, counter;
+	private int sizeX, sizeY, counter=1;
 	private Random rnd = new Random();
 
 	public Generator(int x, int y) {
@@ -32,16 +32,13 @@ public class Generator {
 		for (int i = 1; i < (sizeX); i++) {
 			maze.get(i).setPack(i);
 		}
-		// for(int row=1; row<sizeX; row++){
-		// for(int i=sizeX*row; i<(sizeX*(row+1)); i++){
 		// First line generation
 		for (int i = sizeX; i < sizeX + sizeX; i++) {
 			if ((i % sizeX) == 0) {
-
 				continue;
 			}
-			if ((i % sizeX) == sizeX - 1) { // Setting up the last chunk on
-											// right side
+			// Setting up the last chunk on right side
+			if ((i % sizeX) == sizeX - 1) {
 				maze.get(i).setSide(DIR.RDOWN);
 				if (maze.get(i - 1).getSide() == DIR.RDOWN || maze.get(i - 1).getSide() == DIR.RIGHT) {
 					maze.get(i).setPack(maze.get(i - 1).getPack() + 1);
@@ -52,24 +49,25 @@ public class Generator {
 				}
 				continue;
 			}
+
 			setBorder(i);
+
 		}
 
 		int line = 1;
-//		for(int d=1; d<sizeX-2; d++){
 		copyIntoNextLine(line);
-		display();
+
 		generateNext(line + 1);
-		// }
-		display();
-		line++;
-//		}
+//		displayP();
+//		display();
+		// line++;
 	}
+
 	private void setBorder(int i) {
 		Chunk chunk = maze.get(i);
 		if (rnd.nextBoolean()) { // Randomly set the "|" (true - continue a row,
 									// false - setting "|")
-			if (maze.get(i - 1).getSide() == DIR.RDOWN) {
+			if (maze.get(i - 1).getSide() == DIR.RDOWN || maze.get(i - 1).getSide() == DIR.RIGHT) {
 				chunk.setPack(maze.get(i - 1).getPack() + 1);
 				chunk.setSide(DIR.DOWN);
 				temp.add(chunk);
@@ -79,48 +77,62 @@ public class Generator {
 				temp.add(chunk);
 			}
 		} else {
-			chunk.setSide(DIR.RDOWN);
-			chunk.setPack(maze.get(i - 1).getPack() + 1);
-			makeExit(temp, i);
-	}	}
+			if (maze.get(i - 1).getSide() == DIR.RDOWN || maze.get(i - 1).getSide() == DIR.RIGHT) {
+				chunk.setSide(DIR.RDOWN);
+				chunk.setPack(maze.get(i - 1).getPack() + 1);
+				makeExit(temp, i);
+			} else {
+				chunk.setSide(DIR.RDOWN);
+				chunk.setPack(maze.get(i - 1).getPack());
+				makeExit(temp, i);
+			}
+		}
 
+	}
 
-	
 	private void copyIntoNextLine(int line) {
 		for (int i = (sizeX * line); i < (sizeX * (line + 1)); i++) {
 			maze.get(i + sizeX).setPack(maze.get(i).getPack());
 			maze.get(i + sizeX).setSide(maze.get(i).getSide());
-	}	}
+		}
+	}
 
-
-	
 	private void generateNext(int line) {
 		// Remove "RIGHT" "RDOWM" and their pack numbers
 		for (int i = (sizeX * line); i < (sizeX * (line + 1)); i++) {
 			Chunk chunk = maze.get(i);
+		// Setting the last one	in a row
 			if (i == (sizeX * (line + 1) - 1)) {
-				chunk.setPack(0);
+				chunk.setPack(1);
 				chunk.setSide(DIR.RIGHT);
 				continue;
 			}
+			if(chunk.getSide() == DIR.RDOWN || chunk.getSide() == DIR.RIGHT)
+				counter++;
+		// Making exit
 			if (chunk.getSide() == DIR.DOWN || chunk.getSide() == DIR.RDOWN
-					|| chunk.getSide() == DIR.RIGHT && (i % sizeX) != 0) {
-				chunk.setPack(0);
+					&& (i % sizeX) != 0) {
+				chunk.setPack(counter);
 				chunk.setSide(DIR.EMPTY);
 			}
+			
 		}
+
 		// Filling pack numbers in empty chunks
 		for (int i = (sizeX * line); i < (sizeX * (line + 1)); i++) {
 			Chunk chunk = maze.get(i);
 			if (chunk.getPack() > 0)
 				continue;
-			chunk.setPack(i);
-			if(chunk.getPack() == maze.get(i+1).getPack()){
-				chunk.setSide(DIR.RDOWN);
+		//	chunk.setPack(i);
+		// Cheking if the next chunk is exit in previous row
+			if (chunk.getPack() == maze.get(i + 1).getPack()) {
+//				chunk.setSide(DIR.RDOWN);
 				makeExit(temp, i);
-				}
+			}
 			setBorder(i);
 		}
+		 displayP();
+		 display();
 	}
 
 	// A random exit from particular pack, at least one chunk from pack should
@@ -144,5 +156,14 @@ public class Generator {
 				System.out.println();
 			System.out.printf(maze.get(r).toString());
 		}
+	}
+
+	private void displayP() {
+		for (int r = 0; r < (sizeX) * (sizeY); r++) {
+			if ((r % sizeX) == 0)
+				System.out.println();
+			System.out.printf(String.valueOf(maze.get(r).getPack()));
+		}
+		System.out.println();
 	}
 }
