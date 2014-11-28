73a2173d466b969752bf56108773128b8037f891
Stanislav-Holdenko <Stanislav_Holdenko@epam.com>
2014-11-28 17:12:08 +0200

    Some stuff added

--- src/uk/co/codemachine/mazerun/EilersAlg.java
+++ src/uk/co/codemachine/mazerun/EilersAlg.java
@@ -3,7 +3,7 @@ package uk.co.codemachine.mazerun;
 public class EilersAlg {
 
 	public static void main(String[] args) {
-//		Generator g = new Generator(10, 10);
-		Generator g = new Generator(35, 35);
+		Generator g = new Generator(10, 10);
+//		Generator g = new Generator(35, 35);
 	}
 }
